c65b33bdc3719608692d5a6e1fcd1fcc2c45f451
Thao Phung <thaophung@thaos-air.uwyo.edu>
2015-05-05 21:50:55 -0600

    Initial Commit

---
+++ Playlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playlist
+//
+//  Created by Thao Phung on 5/5/15.
+//  Copyright (c) 2015 Thao Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
